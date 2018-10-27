3605dd52b885fc8a201697b4acefdff9fdf7e8f6
Ben Clifford <benc@hawaga.org.uk>
2018-10-27 17:04:28 +0000

    snake

--- hallow.py
+++ hallow.py
@@ -94,13 +94,138 @@ def set_xrows():
     set_np(xcol,col,(0,0,0))
 
 
+
+# tells us whether the snake will crash
+# on the next move.
+def will_crash(snake, snake_dir):
+  (head_x,head_y) = snake[-1] 
+  (dx,dy) = snake_dir
+
+  next_x = head_x + dx
+  next_y = head_y + dy
+
+  if next_x > 4:
+    return True
+
+  if next_x < 0:
+    return True
+
+  if next_y > 4:
+    return True
+
+  if next_y < 0:
+    return True
+
+  for (x,y) in snake:
+    if x == next_x and y == next_y:
+      return True
+
+  return False
+
+
+def random_start_pos():
+  # avoid starting at an edge, so that we don't
+  # immediately crash
+  return [(random.randint(1,3), random.randint(1,3))]
+
+def run_snake():
+  np.clear()
+
+  snake_dir = (0,1)
+  snake = random_start_pos()
+
+  target_len = 3 
+
+  counts = 0
+
+  go = True
+  while go:
+
+    display.clear()
+
+    np[0] = (255,255,255)
+    for p in range(1,neopixel_count):
+      np[p] = (0,0,0)
+
+    brightness = 9
+    for (x,y) in list(reversed(snake)):
+      display.set_pixel(x,y,brightness)
+      set_np(x,y,(26 * brightness, 5 * brightness, 0))
+      if brightness > 4:
+        brightness = brightness - 1
+    np.show()
+
+    sleep(200)
+
+    # this is the autopilot checking if it will crash
+    # it is allowed to go wrong, just like a player is
+    # allowed to go wrong
+
+    # are we forced to change direction by imminent crash?
+    # or, should we change direction randomly anyway?
+    # (in that second case, the path of not changing direction
+    # if we'd crash on either of our alternatives keeps us
+    # going rather than randomly turning to death)
+
+    random_change = random.randint(0,10) == 7
+
+    if will_crash(snake, snake_dir) or random_change:
+      # We'd better take some evasive action to
+      # not crash.
+      # We have a choice of directions - usually two
+      # and we can check if either of them is any good
+      # and if so, take one of the good ones. 
+    
+      # assume we're going one of the 4 cardinal
+      # directions
+
+      (dx,dy) = snake_dir
+
+      if dx == 0: # then we're going along the y axis
+                  # and are possible positions are along
+                  # the x axis
+
+        if not will_crash(snake, (1,0)):
+          snake_dir = (1,0)
+        elif not will_crash(snake, (-1,0)):
+          snake_dir = (-1,0)
+        # if neither works, give up...
+
+      elif dy == 0:
+        if not will_crash(snake, (0,1)):
+          snake_dir = (0,1)
+        elif not will_crash(snake, (0,-1)):
+          snake_dir = (0,-1)
+
+    # this is the environment checking crashes
+    if will_crash(snake, snake_dir):
+      go = False
+      sleep(1000)
+      np.clear()
+
+    (head_x,head_y) = snake[-1] 
+    (dx,dy) = snake_dir
+
+    snake.append( (head_x + dx, head_y + dy) ) # new head
+
+    if len(snake) > target_len:
+      snake.pop(0) # old tail
+
+    counts = counts + 1
+
+    if counts > 5:
+      counts = 0
+      target_len = target_len + 1
+
+
 while True:
-  c = random.randint(0,2)
-  # c = 2
+  c = random.randint(0,3)
+  # c = 3
   if c == 0:
     set_hrows()
   if c == 1:
     set_vrows()
   if c == 2:
     set_xrows()
-
+  if c == 3:
+    run_snake()
